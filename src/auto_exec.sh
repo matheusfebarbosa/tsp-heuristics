2eab29d30b5347ab4341b852f55c48abfd73d671
Matheus Barbosa <matheusfelipe8496@gmail.com>
2018-10-21 11:03:40 -0300

    Minor changes on build scripts

--- src/auto_exec.sh
+++ src/auto_exec.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 FILES="att48 berlin52 kroA100 kroA150 kroA200 kroB100 kroB150 kroB200 kroC100 kroD100 kroE100 lin105 pr76 pr107 pr124 pr136 pr144 pr152 rat99 rat195 st70"
 
+#make clean
+make
+
 for f in $FILES
 do
   echo "Processing $f file..."
